b743ddb977d2c51393d6d488ac2f1da387827c6a
AdhityaRdt <rahmanadhitya2201@gmail.com>
2018-04-09 15:29:24 +0700

    anchor ini telah selesai

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,41 @@
 "use strict"
+class Animals {
+  constructor(legsNum, isWarmBlooded){
+    this.num_legs = legsNum
+    this.is_warm_blooded = isWarmBlooded
+  }
+}
+
+class Frog extends Animals {
+  constructor(legsNum, isWarmBlooded){
+    super(legsNum, isWarmBlooded)
+  }
+}
+
+class Bat extends Animals {
+  constructor(legsNum, isWarmBlooded){
+    super(legsNum, isWarmBlooded)
+  }
+}
+
+class Chimpanzee extends Animals {
+  constructor(legsNum, isWarmBlooded){
+    super(legsNum, isWarmBlooded)
+  }
+}
+
+class Fox extends Animals {
+  constructor(legsNum, isWarmBlooded){
+    super(legsNum, isWarmBlooded)
+  }
+}
+
+class Chicken extends Animals {
+  constructor(legsNum, isWarmBlooded){
+    super(legsNum, isWarmBlooded)
+  }
+}
+
+let frog = new Frog (2,false)
+let macan = new Animals(4,true)
+console.log(frog);
