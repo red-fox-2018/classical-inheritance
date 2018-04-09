aba03f5f327a519496999a1b5a71fd9eec9850fa
restuu <restu_u@outlook.com>
2018-04-09 16:12:17 +0700

    first commit

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,25 @@
-"use strict"
+class Animals {
+  constructor(legs, blood, fly) {
+    this.num_legs = legs;
+    this.is_warm_blooded = blood;
+    this.can_fly = fly;
+  }
+}
+
+class Frog extends Animals {
+  constructor(legs, blood, fly) {
+    super(legs, blood, fly)
+  }
+}
+
+class Snake extends Animals {
+  constructor(legs, blood, fly) {
+    super(legs, blood, fly)
+  }
+}
+
+
+var frog = new Frog(4, false, false);
+var snake = new Snake(0, false, false)
+console.log(frog);
+console.log(snake);
