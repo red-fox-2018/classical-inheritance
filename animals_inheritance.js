2f91c14c550c62d1996e6e38f70da8db5c7c84b7
Renal Apriansyah <renal.apr@gmail.com>
2018-04-09 04:24:57 -0400

    add classical inheritance task

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,56 +1,50 @@
 class Animal {
-    constructor(num_legs,num_hands){
+    constructor(num_legs){
         this.animal = 'noName'
         this.num_legs = 2
-        this.num_hands = 2
     }
     display(){
-        return `${this.animal} have ${this.num_legs} legs, and have ${this.num_hands} hands`
+        return `${this.animal} have ${this.num_legs} legs`
     }
 }
 
 class Frog extends Animal{
-    constructor(num_legs,num_hands){
+    constructor(num_legs){
         super()
         this.animal = 'frog'
     }
 }
 class Bat extends Animal{
-    constructor(num_legs,num_hands){
+    constructor(num_legs){
         super()
         this.animal = 'bat'
-        this.num_legs = num_legs
-        this.num_hands = num_hands
     }
 }
 class Chimpanze extends Animal{
-    constructor(num_legs,num_hands){
+    constructor(num_legs){
         super()
         this.animal = 'chimpanze'
     }
 }
 class Fox extends Animal{
-    constructor(num_legs,num_hands){
+    constructor(num_legs){
         super()
         this.animal = 'fox'
         this.num_legs = num_legs
-        this.num_hands = num_hands
     }
 }
 class Chicken extends Animal{
-    constructor(num_legs,num_hands){
+    constructor(num_legs){
         super()
         this.animal = 'chicken'
-        this.num_legs = num_legs
-        this.num_hands = num_hands
     }
 }
 
 let frog = new Frog()
-let bat = new Bat(2, 'not')
+let bat = new Bat()
 let chimpanze = new Chimpanze()
-let fox = new Fox(4, 'not')
-let chicken = new Chicken(2, 'not')
+let fox = new Fox(4)
+let chicken = new Chicken()
 
 console.log(frog.display())
 console.log(bat.display())
