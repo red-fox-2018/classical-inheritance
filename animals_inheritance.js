e11244384ae983d5cf1149975a32db679b2c7d5b
bramaprasetyopermana <bramprasetyop@gmail.com>
2018-04-09 19:11:23 +0700

    classical-inheritance

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,36 @@
-"use strict"
+// "use strict"
+class Animal {
+  constructor(num_legs,is_warm_blooded) {
+    this.num_legs= 4
+    this.is_warm_blooded= is_warm_blooded
+  }
+}
+
+class Monkey extends Animal {
+  constructor() {
+    super(2,true)
+    this.num_legs = 2
+  }
+}
+
+class Chicken extends Animal{
+  constructor() {
+    super (2,true)
+    this.num_legs = 2
+  }
+}
+
+class Cow extends Animal {
+  constructor() {
+    super(4,true)
+  }
+}
+
+var animal = new Animal()
+var monkey = new Monkey()
+var chicken = new Chicken()
+var cow = new Cow()
+// console.log(animal);
+console.log(monkey);
+console.log(chicken);
+console.log(cow);
