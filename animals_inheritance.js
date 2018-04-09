d8481987610c9d8fd6c5c3015c607c030e0b3849
Faip Mardoni <faipmardoni@gmail.com>
2018-04-09 16:15:33 +0700

    Challange Inheritance Selesai

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,49 @@
 "use strict"
+class Animal {
+  constructor(num_legs, is_warm_blooded) {
+    this.num_legs = num_legs;
+    this.is_warm_blooded = is_warm_blooded
+  }
+}
+
+class Frog extends Animal {
+  constructor(num_legs, is_warm_blooded) {
+    super(num_legs, is_warm_blooded);
+  }
+}
+
+class Bat extends Animal {
+  constructor(num_legs, is_warm_blooded) {
+    super(num_legs, is_warm_blooded);
+  }
+}
+
+class Chimpanzee extends Animal {
+  constructor(num_legs, is_warm_blooded) {
+    super(num_legs, is_warm_blooded);
+  }
+}
+
+class Fox extends Animal {
+  constructor(num_legs, is_warm_blooded) {
+    super(num_legs, is_warm_blooded);
+  }
+}
+
+class Chicken extends Animal {
+  constructor(num_legs, is_warm_blooded) {
+    super(num_legs, is_warm_blooded);
+  }
+}
+
+const frog = new Frog(2, false);
+const bat = new Bat(2, true);
+const chimpanzee = new Chimpanzee(2, false);
+const fox = new Fox(4, false);
+const chicken = new Chicken(2, false)
+console.log(frog)
+console.log(bat)
+console.log(chimpanzee)
+console.log(fox)
+console.log(chicken)
+
