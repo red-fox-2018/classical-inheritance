b32fc9098f5f431a2b966476047dcfcf427d6fd1
annisaayu <annisaayu569@gmail.com>
2018-04-09 18:48:12 +0700

    inheritance

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,57 @@
 "use strict"
+class Animal{
+  constructor(numLegs, isMammals){
+    this.num_legs = numLegs
+    this.is_warm_blood = true
+    this.is_mammals = isMammals
+  }
+
+  superpower(){
+    return new SuperPower()
+  }
+}
+
+class Cow extends Animal {
+  constructor() {
+    super()
+  }
+}
+
+class Fox extends Animal {
+  constructor() {
+    super()
+  }
+}
+
+class Frog extends Animal {
+  constructor(num_legs, is_mammals) {
+    super(num_legs, is_mammals)
+  }
+}
+
+class Duck extends Animal {
+  constructor(num_legs, is_mammals) {
+    super(num_legs, is_mammals)
+  }
+
+}
+
+class SuperPower {
+  constructur(){
+
+  }
+
+  use_laser_vision(){
+    console.log('Serangan laseeer......');
+  }
+
+  be_invisible(){
+    console.log('Aku menghilang....');
+  }
+
+}
+
+//===========Driver Code========
+let duck = new Duck(2, false)
+console.log(duck);
+duck.superpower().use_laser_vision();
