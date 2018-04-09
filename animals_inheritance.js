253069f9587006759b5d660925718ff93af1b60a
alprak <you@example.com>
2018-04-09 21:05:55 +0700

    done

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,40 @@
 "use strict"
+class Animal {
+	constructor(num_legs,is_warm_bloded) {
+		this.num_legs=num_legs;
+		this.is_warm_bloded=is_warm_bloded;
+	}
+}
+
+
+class Frog extends Animal  {
+	constructor() {
+		super(2,false);
+		this.num_legs=4;
+		this.is_warm_bloded=true;
+	}
+}
+
+class Cat extends Animal {
+	constructor() {
+		super(3,false);
+		this.is_warm_bloded=true;
+		
+	}
+}
+
+class Horse extends Animal {
+	constructor() {
+		super(5,false);
+		this.num_legs=4;
+		
+	}
+}
+
+
+var frog=new Frog();
+var cat= new Cat();
+var horse=new Horse();
+console.log(frog);
+console.log(cat);
+console.log(horse);
